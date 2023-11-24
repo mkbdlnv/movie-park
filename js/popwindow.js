8efeb2e0e270f226e4da185f38cd2a15d2d51467
FlakWorld <122820173+FlakWorld@users.noreply.github.com>
2023-11-25 04:46:49 +0600

    Update popwindow.js

--- js/popwindow.js
+++ js/popwindow.js
@@ -4,11 +4,9 @@ function toggleChat() {
     chatPopup.style.display = (chatPopup.style.display === 'none' || chatPopup.style.display === '') ? 'block' : 'none';
 }
 
-// Функция для отправки сообщения (в данном примере просто выводим в консоль)
+
 function sendMessage() {
     var messageInput = document.getElementById('messageInput');
     var message = messageInput.value;
-    console.log('Отправлено сообщение: ' + message);
-    // Перенаправление на другую страницу (замените 'новая-страница.html' на ваш URL)
     window.location.href = "helpform.html";
 }
